e53331d13d850ebcc192db13e160c93473fcfc38
Nolan <nolan.king90@gmail.com>
2024-12-29 21:31:05 -0500

    make test data

--- crates/trainer/src/main.rs
+++ crates/trainer/src/main.rs
@@ -1,12 +1,13 @@
 use parser::{parse_latex, SyntaxConfig};
 use smartcore::linalg::basic::matrix::DenseMatrix;
 use smartcore::tree::decision_tree_classifier::{
-    DecisionTreeClassifier, DecisionTreeClassifierParameters,
+    DecisionTreeClassifier, DecisionTreeClassifierParameters, SplitCriterion,
 };
 use std::fs;
 use std::io;
 use std::path::PathBuf;
 use syntax::latex::{SyntaxElement, SyntaxKind, SyntaxNode};
+use xxhash_rust::xxh3::xxh3_64;
 
 fn main() -> io::Result<()> {
     let examples_dir = "texExamples";
@@ -56,17 +57,18 @@ fn main() -> io::Result<()> {
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum FormatDecision {
-    NoBreak,       // no space at all
-    Space,         // a single space
-    NewLine(bool), // break + indent
+    NoBreak,      // no space at all
+    Space,        // a single space
+    NewLine(u32), // break + indent
+    SkipLine(u32),
 }
 
 fn decision_to_integer(d: &FormatDecision) -> u32 {
     match d {
         FormatDecision::NoBreak => 0,
         FormatDecision::Space => 1,
-        FormatDecision::NewLine(true) => 2,
-        FormatDecision::NewLine(false) => 3,
+        FormatDecision::NewLine(num) => 2 * (*num + 1), // even numbers >= 2
+        FormatDecision::SkipLine(num) => 2 * (*num + 1) + 1, // odd numbers >= 3
     }
 }
 
@@ -74,38 +76,63 @@ fn label_to_decision(label: u32) -> FormatDecision {
     match label {
         0 => FormatDecision::NoBreak,
         1 => FormatDecision::Space,
-        2 => FormatDecision::NewLine(true),
-        3 => FormatDecision::NewLine(false),
-        // fallback
-        _ => FormatDecision::NoBreak,
+        _ => {
+            if label % 2 == 0 {
+                let num = (label / 2) - 1;
+                FormatDecision::NewLine(num)
+            } else {
+                let num = (label - 1) / 2 - 1;
+                FormatDecision::SkipLine(num)
+            }
+        }
     }
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Features {
     kind: SyntaxKind,
+    name: String,
     parent_kind: Option<SyntaxKind>,
+    l_sibling_kind: Option<SyntaxKind>,
+    r_sibling_kind: Option<SyntaxKind>,
     is_section_command: bool,
     token_length: usize, // 0 for a node
     nesting_depth: usize,
     is_math_mode: bool,
     is_aligned_env: bool, // align, array, and matrix envs., and tabular type
+    num_children: usize,
+    num_siblings: usize,
 }
 
 fn features_to_vec(f: &Features) -> Vec<f32> {
     vec![
         kind_to_f32(&f.kind),
+        string_to_hash(&f.name) as f32,
         optional_kind_to_f32(f.parent_kind),
+        optional_kind_to_f32(f.l_sibling_kind),
+        optional_kind_to_f32(f.r_sibling_kind),
         f.is_section_command as u8 as f32,
         f.token_length as f32,
         f.nesting_depth as f32,
         f.is_math_mode as u8 as f32,
         f.is_aligned_env as u8 as f32,
+        f.num_children as f32,
+        f.num_siblings as f32,
     ]
 }
 
 fn extract_features(element: &SyntaxElement) -> Features {
     let parent_kind = element.parent().map(|parent| parent.kind());
+    let r_sibling_kind = element.next_sibling_or_token().map(|sib| sib.kind());
+    let l_sibling_kind = element.prev_sibling_or_token().map(|sib| sib.kind());
+    let num_siblings = match element.parent() {
+        Some(parent) => parent.children().count(),
+        None => 0
+    };
+    let num_children = match element.as_node() {
+        Some(node) => node.children().count(),
+        None => 0
+    };
 
     let is_section_command = matches!(
         element.kind(),
@@ -122,16 +149,37 @@ fn extract_features(element: &SyntaxElement) -> Features {
     };
 
     let nesting_depth = element.ancestors().count();
+    let mut name = String::new();
+    if matches!(element.kind(), SyntaxKind::ENVIRONMENT) {
+       name = get_env_name(element.as_node().unwrap());
+    }
+    if matches!(element.kind(), SyntaxKind::GENERIC_COMMAND) {
+        name = get_command_name(element.as_node().unwrap());
+    }
 
     Features {
         kind: element.kind(),
+        name,
         parent_kind,
+        l_sibling_kind,
+        r_sibling_kind,
         is_section_command,
         token_length,
         nesting_depth,
         is_math_mode: is_math_mode(element),
         is_aligned_env: is_aligned_env(element),
+        num_children,
+        num_siblings,
     }
+}
+
+fn get_command_name(node: &SyntaxNode) -> String {
+    let name = node.first_child_or_token().unwrap().to_string();
+    name
+}
+
+fn string_to_hash(input: &str) -> u64 {
+    xxh3_64(input.as_bytes())
 }
 
 fn is_math_mode(element: &SyntaxElement) -> bool {
@@ -219,12 +267,21 @@ fn extract_label(element: &SyntaxElement) -> FormatDecision {
     match element.next_sibling_or_token() {
         Some(SyntaxElement::Token(token)) => {
             if token.to_string() == *"\n" {
-                match token.next_token() {
-                    Some(tkn) => match tkn.to_string().as_str() {
-                        "\t" => FormatDecision::NewLine(true),
-                        _ => FormatDecision::NewLine(false),
-                    },
-                    None => FormatDecision::NewLine(false),
+                let mut indents = 0;
+                let mut lines = 0;
+                let mut tkn = token.next_token();
+                while tkn.is_some() {
+                    if matches!(tkn.clone().unwrap().text(), "\t" | " ") {
+                        indents += 1;
+                    } else if matches!(tkn.clone().unwrap().text(), "\n") {
+                        lines += 1;
+                    }
+                    tkn = tkn.unwrap().next_token();
+                }
+                if indents > 0 {
+                    FormatDecision::NewLine(2 * (indents + 1))
+                } else {
+                    FormatDecision::SkipLine(2 * (lines + 1) + 1)
                 }
             } else if token.to_string() == *" " {
                 FormatDecision::Space
@@ -284,7 +341,7 @@ fn dataset_to_matrix(dataset: &[TrainingExample]) -> (DenseMatrix<f32>, Vec<u32>
     }
 
     // We know how many features per example we used:
-    let n_features = 7; // in our example (kind_val, parent_val, etc.)
+    let n_features = 12; // in our example (kind_val, parent_val, etc.)
     let n_samples = dataset.len();
 
     let x = DenseMatrix::new(n_samples, n_features, data, false).unwrap();
@@ -296,13 +353,19 @@ pub struct Classifier(DecisionTreeClassifier<f32, u32, DenseMatrix<f32>, Vec<u32
 pub fn train(dataset: &[TrainingExample]) -> Classifier {
     let (x, y) = dataset_to_matrix(dataset);
 
-    let params = DecisionTreeClassifierParameters::default().with_max_depth(10);
+    let params = DecisionTreeClassifierParameters::default().with_criterion(SplitCriterion::Gini);
 
     Classifier(DecisionTreeClassifier::fit(&x, &y, params).expect("Failed to train decision tree"))
 }
 
 pub fn predict(x: &DenseMatrix<f32>, classifier: &Classifier) -> Vec<FormatDecision> {
-    classifier.0.predict(x).unwrap().iter().map(|&pred| label_to_decision(pred)).collect()
+    classifier
+        .0
+        .predict(x)
+        .unwrap()
+        .iter()
+        .map(|&pred| label_to_decision(pred))
+        .collect()
 }
 
 pub fn visit(element: &SyntaxElement, prediction: &mut Vec<FormatDecision>) -> String {
@@ -311,15 +374,23 @@ pub fn visit(element: &SyntaxElement, prediction: &mut Vec<FormatDecision>) -> S
     match element {
         SyntaxElement::Token(token) => {
             output.push_str(&token.to_string());
-            println!("{}", &token.to_string());
-            println!("{:?}", pred);
+            //println!("{}", &token.to_string());
+            //println!("{:?}", pred);
             match pred {
-                FormatDecision::Space => { output.push(' '); },
-                FormatDecision::NoBreak => {},
-                FormatDecision::NewLine(true) => { output.push_str("\n  "); }
-                FormatDecision::NewLine(false) => { output.push('\n'); }
+                FormatDecision::Space => {
+                    output.push(' ');
+                }
+                FormatDecision::NoBreak => {}
+                FormatDecision::NewLine(indent_level) => {
+                    output.push_str("\n");
+                    let indents = "\t".to_string().repeat((indent_level / 2 - 1) as usize);
+                    output.push_str(&indents);
+                }
+                FormatDecision::SkipLine(lines) => {
+                    output.push_str(&"\n".repeat(((lines - 1) / 2 - 1) as usize));
+                }
             }
-        },
+        }
         SyntaxElement::Node(node) => {
             for child in node.children_with_tokens() {
                 output.push_str(&visit(&child, prediction));
